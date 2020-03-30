2baa5b698731ddb78f2bd36e36d863d5b5d0c376
simonojok19 <simonojok19@gmail.com>
2020-03-30 10:02:25 +0300

    implemented the app widgte

--- lib/main.dart
+++ lib/main.dart
@@ -2,3 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: Home(),
+    );
+  }
+}
+
