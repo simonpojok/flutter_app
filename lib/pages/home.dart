8da7c938034dc48c3bb41e8861a6e1ce27e2d611
simonojok19 <simonojok19@gmail.com>
2020-03-30 11:17:26 +0300

    added the const constructor to the widget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -57,6 +57,7 @@ class _HomeState extends State<Home> {
 }
 
 class ContainerWithBoxDecorationWidget extends StatelessWidget{
+  const ContainerWithBoxDecorationWidget({ Key key, }): super(key: key);
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
