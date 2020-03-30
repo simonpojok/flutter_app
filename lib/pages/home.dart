9aaa7d15a2c0978b5eaba6df291cf1a303a6ad11
simonojok19 <simonojok19@gmail.com>
2020-03-30 18:01:49 +0300

    done implementing the row and columm nesting

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -67,7 +67,25 @@ class ColumnAndRowNestingWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Text('Columns and Row Nesting 1'),
+        Text('Columns and Row Nesting 2'),
+        Text('Columns and Row Nesting 3'),
+        Padding(padding: EdgeInsets.all(16.0),),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text('Row Nesting 1'),
+            Text('Row Nesting 2'),
+            Text('Row Nesting 3')
+          ],
+        ),
+      ],
+    );
   }
   
 }
