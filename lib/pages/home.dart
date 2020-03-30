22470b3b99f054413770e538c4a6078b85d2aec4
simonojok19 <simonojok19@gmail.com>
2020-03-30 11:07:49 +0300

    Container Step 1

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -46,7 +46,7 @@ class _HomeState extends State<Home> {
           child: SingleChildScrollView(
             child: Column(
               children: <Widget>[
-                
+                const ContainerWithBoxDecorationWidget()
               ],
             ),
           ),
