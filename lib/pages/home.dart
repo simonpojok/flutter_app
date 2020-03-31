e165e9fdd2f2674b5c2b2f8621a5b66e40233c46
simonojok19 <simonojok19@gmail.com>
2020-03-31 10:12:20 +0300

    fully implemented the build method

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -216,8 +216,33 @@ class PopupMenuButtonWidget extends StatelessWidget implements PreferredSizeWidg
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return Container(
+      color: Colors.lightGreen.shade100,
+      height: preferredSize.height,
+      width: double.infinity,
+      child: Center(
+        child: PopupMenuButton<TodoMenuItem>(
+          icon: Icon(Icons.view_list),
+          onSelected: ((valueSelected) {
+            print('valueSelected: ${valueSelected.title}');
+          }),
+          itemBuilder: (BuildContext context) {
+            return foodMenuList.map((TodoMenuItem todoMenuItem) {
+              return PopupMenuItem<TodoMenuItem> (
+                value: todoMenuItem,
+                child: Row(
+                  children: <Widget>[
+                    Icon(todoMenuItem.icon.icon),
+                    Padding(padding: EdgeInsets.all(8.0),),
+                    Text(todoMenuItem.title)
+                  ],
+                ),
+              );
+            }).toList();
+          },
+        ),
+      ),
+    );
   }
 
   @override
