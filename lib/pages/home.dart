fdc88e16e7425415bc17b2c2c569bf835223e5a5
simonojok19 <simonojok19@gmail.com>
2020-04-01 17:15:42 +0300

    Step 6: added the created widget to the children array of the column widget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,7 +14,7 @@ class _HomeState extends State<Home> {
       body: SafeArea(
         child: Column(
           children: <Widget>[
-            
+            AnimatedContainerWidget()
           ],
         ),
       ),
