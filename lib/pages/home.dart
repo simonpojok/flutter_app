4e7cb5141ba42f98da4e42386df6d229f683c2fc
simonojok19 <simonojok19@gmail.com>
2020-04-01 15:02:42 +0300

    finished the layout

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -42,7 +42,17 @@ class OrientationBuilderWidget extends StatelessWidget {
       builder: (BuildContext context, Orientation orientation) {
         return orientation == Orientation.portrait ? Container(
           alignment: Alignment.center,
-        ): 
+          color: Colors.yellow,
+          height: 100.0,
+          width: 100.0,
+          child: Text('Portrait'),
+        ): Container(
+          alignment: Alignment.center,
+          color: Colors.lightGreen,
+          height: 100.0,
+          width: 200.0,
+          child: Text('Lanscape'),
+        )
       },
     );
   }
