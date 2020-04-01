0002d6ae1bc6af924d0700d63e2d5139f221f372
simonojok19 <simonojok19@gmail.com>
2020-04-01 14:38:10 +0300

    added orienations using container widget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -33,7 +33,19 @@ class OrientationLayoutWidget extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     Orientation _orientation = MediaQuery.of(context).orientation;
-    return _orientation == Orientation.portrait? Row() : Row();
+    return _orientation == Orientation.portrait? Container(
+      alignment: Alignment.center,
+      color: Colors.yellow,
+      height: 100.0,
+      width: 100.0,
+      child: Text('Portrait'),
+    ) : Container(
+      alignment: Alignment.center,
+      color: Colors.lightGreen,
+      height: 100.0,
+      width: 200.0,
+      child: Text('Landscape'),
+    );
   }
 }
 
