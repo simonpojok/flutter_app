41765c69c43051ff1174289d1c67eb78824d4a8f
simonojok19 <simonojok19@gmail.com>
2020-04-01 16:54:51 +0300

    Step2: added the widget

--- lib/widgets/animated_container.dart
+++ lib/widgets/animated_container.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class AnimatedContainerWidget extends StatefulWidget {
+  @override
+  _AnimatedContainerWidgetState createState() => _AnimatedContainerWidgetState();
+}
+
+class _AnimatedContainerWidgetState extends State<AnimatedContainerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
