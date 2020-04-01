7301c8f338510de81610c9ff22a110d66258a4ef
simonojok19 <simonojok19@gmail.com>
2020-04-01 18:17:43 +0300

    Step 2: Imported material dart and created the methods

---
+++ lib/widgets/animated_opacity.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnimatedOpacityWidget extends StatefulWidget {
+  @override
+  _AnimatedOpacityWidgetState createState() => _AnimatedOpacityWidgetState();
+}
+
+class _AnimatedOpacityWidgetState extends State<AnimatedOpacityWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
