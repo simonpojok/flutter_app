18a0205315afecb4604dc33721b87eeee6676bdd
simonojok19 <simonojok19@gmail.com>
2020-04-01 17:33:10 +0300

    added the a method and variable of type boolean

--- lib/widgets/animated_cross_fade.dart
+++ lib/widgets/animated_cross_fade.dart
@@ -7,6 +7,12 @@ class AnimatedCrossFadeWidget extends StatefulWidget {
 }
 
 class _AnimatedCrossFadeWidgetState extends State<AnimatedCrossFadeWidget> {
+  bool _crossFadeStateShowFirst = true;
+  _crossFade() {
+    setState(() {
+      
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
