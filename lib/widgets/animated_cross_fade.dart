afe24297061fce71b1abe0caf363279b1218c4b2
simonojok19 <simonojok19@gmail.com>
2020-04-01 17:31:02 +0300

    Step 2: Created animated cross fad widget class

--- lib/widgets/animated_cross_fade.dart
+++ lib/widgets/animated_cross_fade.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+
+
+class AnimatedCrossFadeWidget extends StatefulWidget {
+  @override
+  _AnimatedCrossFadeWidgetState createState() => _AnimatedCrossFadeWidgetState();
+}
+
+class _AnimatedCrossFadeWidgetState extends State<AnimatedCrossFadeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
