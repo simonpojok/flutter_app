74e61ec7c972643d2d5fa58370f9cae4334ce6d6
simonojok19 <simonojok19@gmail.com>
2020-04-02 18:50:33 +0300

    added menu list tile widget

--- lib/widgets/menu_list_tile.dart
+++ lib/widgets/menu_list_tile.dart
@@ -2,3 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/pages/birthdays.dart';
 import 'package:flutter_app/pages/gratitude.dart';
 import 'package:flutter_app/pages/reminders.dart';
+
+class MenuListTileWidget extends StatefulWidget {
+  @override
+  _MenuListTileWidgetState createState() => _MenuListTileWidgetState();
+}
+
+class _MenuListTileWidgetState extends State<MenuListTileWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
