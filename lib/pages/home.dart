4238021c527241ff12ff640abc57d59e94a5bc2d
simonojok19 <simonojok19@gmail.com>
2020-04-02 16:11:51 +0300

    Step 5: withed singletickerprovidermixin

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,7 +8,7 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
