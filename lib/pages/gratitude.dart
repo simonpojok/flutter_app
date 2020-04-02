749110d549c55a8c5ce5400b12337e59b92cbe5a
simonojok19 <simonojok19@gmail.com>
2020-04-02 11:04:00 +0300

    Step 3: created the gratitude constructor

--- lib/pages/gratitude.dart
+++ lib/pages/gratitude.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Gratitude extends StatefulWidget {
+  final int radioGroupValue;
+  Gratitude({ Key key, @required this.radioGroupValue }): super(key: key);
   @override
   _GratitudeState createState() => _GratitudeState();
 }
