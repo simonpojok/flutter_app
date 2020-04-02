6d9719edba1f66178db852a085a6b4c9a619b4b9
simonojok19 <simonojok19@gmail.com>
2020-04-02 13:18:08 +0300

    Step 8: Fully implemented the fry widget

--- lib/pages/fly.dart
+++ lib/pages/fly.dart
@@ -6,7 +6,17 @@ class Fly extends StatelessWidget {
   Widget build(BuildContext context) {
     double _width = MediaQuery.of(context).size.shortestSide /2;
     return Scaffold(
-      body: Hero(),
+      body: Hero(
+        tag: 'format_paint',
+        child: Container(
+          alignment: Alignment.bottomCenter,
+          child: Icon(
+            Icons.format_paint,
+            color: Colors.lightGreen,
+            size: _width
+          ),
+        ),
+      ),
     );
   }
 }
