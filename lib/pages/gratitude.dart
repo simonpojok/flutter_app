170d03d02879c9dc059cc4f2a983bb9be1873e4c
simonojok19 <simonojok19@gmail.com>
2020-04-02 11:07:13 +0300

    Step 4: added the apbar

--- lib/pages/gratitude.dart
+++ lib/pages/gratitude.dart
@@ -10,8 +10,16 @@ class Gratitude extends StatefulWidget {
 class _GratitudeState extends State<Gratitude> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gratitude'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.check),
+            onPressed: () => Navigator.pop(context, _selectedGratitude),
+          ),
+        ],
+      ),
     );
   }
 }
