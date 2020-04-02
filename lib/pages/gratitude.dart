d95da9d7fa8ab09ca5c3a111f45f5bebfe1439b5
simonojok19 <simonojok19@gmail.com>
2020-04-02 11:01:58 +0300

    Step 3: Created stateful gratitude

---
+++ lib/pages/gratitude.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Gratitude extends StatefulWidget {
+  @override
+  _GratitudeState createState() => _GratitudeState();
+}
+
+class _GratitudeState extends State<Gratitude> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
