390af8aec58bd2f81d77e3a11e2bc0badeee2a35
simonojok19 <simonojok19@gmail.com>
2020-04-02 19:18:19 +0300

    Step 13: added left drawer widget

---
+++ lib/widgets/left_drawer.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/widgets/menu_list_tile.dart';
+
+class LeftDrawerWidget extends StatefulWidget {
+  @override
+  _LeftDrawerWidgetState createState() => _LeftDrawerWidgetState();
+}
+
+class _LeftDrawerWidgetState extends State<LeftDrawerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
