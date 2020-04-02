3cb88163264dfc09fa9d927ae3f6cad978ce8a61
simonojok19 <simonojok19@gmail.com>
2020-04-02 14:24:56 +0300

    Step 11: added codes for reminders.dart

---
+++ lib/pages/reminders.dart
@@ -0,0 +1,17 @@
+// reminders.dart
+import 'package:flutter/material.dart';
+
+class Reminders extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Icon(
+          Icons.access_alarm,
+          size: 120.0,
+          color: Colors.purple,
+        ),
+      ),
+    );
+  }
+}
