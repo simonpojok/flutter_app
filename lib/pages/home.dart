07847379edf62d50c275570d78b248f6c5f59875
simonojok19 <simonojok19@gmail.com>
2020-04-02 10:07:41 +0300

    Step 2: Fully implemented the appbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,7 +10,13 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
+        title: Text('Navigator'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.info_outline),
+            onPressed: () => _openPageAbout(context: context, fullscreenDialog: true,),
+          ),
+        ],
       ),
     );
   }
