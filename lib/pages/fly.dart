db2ecefb5aa740292d65fc27d0e62dad59adbacf
simonojok19 <simonojok19@gmail.com>
2020-04-02 13:15:27 +0300

    Step 7: created the screen size variable

--- lib/pages/fly.dart
+++ lib/pages/fly.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class Fly extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
+    double _width = MediaQuery.of(context).size.shortestSide /2;
     return Scaffold(
       body: Hero(),
     );
