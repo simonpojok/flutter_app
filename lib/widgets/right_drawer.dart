83112c18aa058278493bb2e000a8d5e81b18fa78
simonojok19 <simonojok19@gmail.com>
2020-04-02 19:39:01 +0300

    Step 17: added the right drawer widget

--- lib/widgets/right_drawer.dart
+++ lib/widgets/right_drawer.dart
@@ -1,2 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/widgets/menu_list_tile.dart';
+
+class RightDrawerWidget extends StatefulWidget {
+  @override
+  _RightDrawerWidgetState createState() => _RightDrawerWidgetState();
+}
+
+class _RightDrawerWidgetState extends State<RightDrawerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
