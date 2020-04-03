0d31ec99a7a53ea22ae6b932dcd0fa0a6a33cc22
simonojok19 <simonojok19@gmail.com>
2020-04-03 22:07:47 +0300

    Step 20: added the slivergridwidget

---
+++ lib/widgets/sliver_grid.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SliverGridWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
--- lib/widgets/sliver_list.dart
+++ lib/widgets/sliver_list.dart
@@ -5,7 +5,20 @@ class SliverListWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SliverList(
-      delegate: SliverChildListDelegate(),
+      delegate: SliverChildListDelegate(
+        List.generate(3, (int index) {
+          return ListTile(
+            leading: CircleAvatar(
+              child: Text('${index + 1 }'),
+              backgroundColor: Colors.lightGreen,
+              foregroundColor: Colors.white,
+            ),
+            title: Text('Row ${ index + 1 }'),
+            subtitle: Text('Subtitle Row ${ index + 1 }'),
+            trailing: Icon(Icons.star_border),
+          );
+        })
+      ),
     );
   }
 }
