cf90c38a2d833f4ea0dc2db5e87f0eb54378ffbd
simonojok19 <simonojok19@gmail.com>
2020-04-03 19:54:34 +0300

    Step 18: added the positioned widget

--- lib/widgets/stack_favorite.dart
+++ lib/widgets/stack_favorite.dart
@@ -7,6 +7,29 @@ class StackFavoriteWidget extends StatelessWidget {
       color: Colors.black87,
       child: Padding(
         padding: EdgeInsets.all(16.0),
+        child: Stack(
+          children: <Widget>[
+            Image(image: AssetImage('assets/images/dawn.jpg'),),
+            Positioned(
+              bottom: 0.0,
+              right: 0.0,
+              child: FractionalTranslation(
+                translation: Offset(0.3, -0.3),
+                child: CircleAvatar(
+
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 10.0,
+              right: 10.0,
+              child: CircleAvatar(
+                radius: 48.0,
+                backgroundImage: AssetImage('assets/images/eagle.jpg'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
