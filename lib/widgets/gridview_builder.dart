7dbf9cd33897e80ad91f9e794c1cc6baa7086315
simonojok19 <simonojok19@gmail.com>
2020-04-03 12:49:36 +0300

    Step 10: completed here done

--- lib/widgets/gridview_builder.dart
+++ lib/widgets/gridview_builder.dart
@@ -4,6 +4,8 @@ class GridViewBuilderWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
+      itemCount: 20,
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 150.0),
       
     );
   }
