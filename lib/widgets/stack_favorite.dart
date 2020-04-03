e53c164b9186972f452047ddf91e8356c19ccfdd
simonojok19 <simonojok19@gmail.com>
2020-04-03 19:47:18 +0300

    Step 15: complete

--- lib/widgets/stack_favorite.dart
+++ lib/widgets/stack_favorite.dart
@@ -4,7 +4,10 @@ class StackFavoriteWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      color: Colors.black87,
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+      ),
     );
   }
 }
