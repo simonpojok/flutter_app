7edef89cd6ec18f3d271fe51b20a3392a3fd09b9
simonojok19 <simonojok19@gmail.com>
2020-04-03 11:43:30 +0300

    Step 10: fully implemented the stuff well

--- lib/widgets/row_with_card.dart
+++ lib/widgets/row_with_card.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
 class RowWithCardWidget extends StatelessWidget {
+  final int index;
+  const RowWithCardWidget({ Key key, @required this.index }): super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Card(
+      child: ListTile(
+        leading: Icon(
+          Icons.flight,
+          size: 48.0,
+          color: Colors.lightBlue,
+        ),
+        title: Text('Airplane $index'),
+        subtitle: Text('Very Cool'),
+        trailing: Text(
+          '${ index * 7 }%',
+          style: TextStyle(color: Colors.lightBlue),
+        ),
+        onTap: () {
+          print('Tapped on Row $index');
+        },
+      ),
     );
   }
 }
