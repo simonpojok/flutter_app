b04a9a129c5b54228cd70a50456f0df2305d8640
simonojok19 <simonojok19@gmail.com>
2020-04-03 21:34:50 +0300

    Step 10: added the return

--- lib/widgets/sliver_app_bar.dart
+++ lib/widgets/sliver_app_bar.dart
@@ -4,7 +4,7 @@ class SliverAppBarWidget extends StatelessWidget {
   const SliverAppBarWidget({ Key key, }): super( key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SliverAppBar(
       
     );
   }
