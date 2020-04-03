549a04429fb94f404e56f925aa9199cc19ff6126
simonojok19 <simonojok19@gmail.com>
2020-04-03 11:50:07 +0300

    completed

--- lib/widgets/row.dart
+++ lib/widgets/row.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
 class RowWidget extends StatelessWidget {
+  final int index;
+  const RowWidget({ Key key, @required this.index }): super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return ListTile(
+      leading: Icon(
+        Icons.directions_car,
+        size: 48.0,
+        color: Colors.lightGreen
+      ),
+      title: Text('Car $index'),
+      subtitle: Text('Very Cool'),
+      trailing: (index % 3).isEven ? Icon(Icons.bookmark_border): Icon(Icons.bookmark),
+      selected: false,
+      onTap: () {
+        print('Tapped on Row $index');
+      },
     );
   }
 }
