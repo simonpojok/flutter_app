790c685ca59c1e7f9bbc56dc4cf702e5918cefbb
simonojok19 <simonojok19@gmail.com>
2020-04-03 14:39:52 +0300

    Step 5: added safeare with a listview builder into the body of scaffold

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(title: Text('Stack Widget'),),
+      body: SafeArea(
+        child: ListView.builder(),
+      ),
     );
   }
 }
