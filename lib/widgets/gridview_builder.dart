ee0bb7de3c6ad40329542dba65cddcfc80c06d6f
simonojok19 <simonojok19@gmail.com>
2020-04-03 13:00:34 +0300

    complete

--- lib/widgets/gridview_builder.dart
+++ lib/widgets/gridview_builder.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/classes/gridview_builder.dart';
 
 class GridViewBuilderWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<IconData> _iconList = GridIcons().getIconList();
     return GridView.builder(
       itemCount: 20,
+      padding: EdgeInsets.all(8.0),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 150.0),
-      
+      itemBuilder: (BuildContext context, int index) {
+        print('_buildGridViewBuilder $index');
+        return Card(
+          color: Colors.lightGreen.shade50,
+          margin: EdgeInsets.all(8.0),
+          child: InkWell(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  _iconList[index],
+                  size: 48.0,
+                  color: Colors.lightGreen
+                ),
+                Divider(),
+                Text(
+                  'Index $index',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ],
+            ),
+            onTap: () {
+              print('Row $index');
+            },
+          ),
+        );
+      },
     );
   }
 }
