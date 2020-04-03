967e0c1ef288371361b91b0509681a3d8d62eb0e
simonojok19 <simonojok19@gmail.com>
2020-04-03 11:12:03 +0300

    Step 2: add the listview

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(title: Text('List')),
+      body: SafeArea(
+        child: ListView.builder(itemBuilder: null),
+      ),
     );
   }
 }
