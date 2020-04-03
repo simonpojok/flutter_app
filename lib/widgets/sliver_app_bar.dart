fa9968d4430f58f9ffe7af4d76b2f9343a039328
simonojok19 <simonojok19@gmail.com>
2020-04-03 21:33:49 +0300

    Step 9: add the const constructor

--- lib/widgets/sliver_app_bar.dart
+++ lib/widgets/sliver_app_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SliverAppBarWidget extends StatelessWidget {
+  const SliverAppBarWidget({ Key key, }): super( key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
