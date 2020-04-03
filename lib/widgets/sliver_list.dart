7d5494f93dc12f716225449f34f43544bf73a0e2
simonojok19 <simonojok19@gmail.com>
2020-04-03 21:55:50 +0300

    added sliver list deligate

--- lib/widgets/sliver_list.dart
+++ lib/widgets/sliver_list.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class SliverListWidget extends StatelessWidget {
+  const SliverListWidget({ Key key }): super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return SliverList(
+      delegate: SliverChildListDelegate(),
     );
   }
 }
