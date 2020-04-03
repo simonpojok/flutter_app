b2ec3dbcea9b9c6f39aa02d6dff71a7615bd8104
simonojok19 <simonojok19@gmail.com>
2020-04-03 12:45:49 +0300

    Step: state 9 done

--- lib/widgets/gridview_builder.dart
+++ lib/widgets/gridview_builder.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class GridViewBuilderWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return GridView.builder(
       
     );
   }
