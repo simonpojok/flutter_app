e98adb0ad7c10086efec3d246c28919851180040
simonojok19 <simonojok19@gmail.com>
2020-04-03 11:30:11 +0300

    Step 7: fully implemented the codes

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 
 class HeaderWidget extends StatelessWidget {
+  final int index;
+  const HeaderWidget({ Key key, @required this.index}): super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      padding: EdgeInsets.all(16.0),
+      height: 120.0,
+      child: Card(
+        elevation: 8.0,
+        color: Colors.white,
+        shape: StadiumBorder(),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Barista',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 48.0,
+                color: Colors.orange
+              ),
+            ),
+            Text(
+              'Travel Plans',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.grey),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
