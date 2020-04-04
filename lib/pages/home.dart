2109048d583470438642f8e63b3b7e41dd1000e2
simonojok19 <simonojok19@gmail.com>
2020-04-04 09:58:39 +0300

    Step 5: complete

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,8 +3,21 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Layout',
+          style: TextStyle(color: Colors.black87),
+        ),
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black54),
+        brightness: Brightness.light,
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: () { },),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.cloud_queue), onPressed: () { },)
+        ],
+      ),
+      body: _buildBody(),
     );
   }
 }
